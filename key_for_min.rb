1c9d5a54a7e965626f4b32077801496336e60ce9
Aaron Muesing <muesing.music@gmail.com>
2021-01-04 19:58:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = 1/0.0
-  name_hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
+  name_hash.each do |key, value|
+    if value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
   end
   lowest_key
